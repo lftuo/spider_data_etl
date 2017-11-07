5e92642d81d2856583bca84dae25c04938802c8a
lftuo <909709223@qq.com>
2017-11-07 19:59:46 +0800

    上传最新代码

--- updata_model_normal.py
+++ updata_model_normal.py
@@ -16,6 +16,7 @@ sys.setdefaultencoding('utf-8')
 class UpdateModel(object):
 
     def __update_data__(self,table_name):
+
         # 读取models手机型号表单
         conn = Util().get_db_conn()
         cur = conn.cursor()
@@ -29,19 +30,6 @@ class UpdateModel(object):
             brand_chinese = line[3]
             print model,brand,cnt,brand_chinese
             update.__update_model_recursion__(model,brand_chinese,table_name)
-        """
-        try:
-            file = open(path,'r')
-            while 1:
-                model = file.readline().strip('\n').strip()
-                update.__update_model_recursion__(model)
-                if not model:
-                    break
-                pass
-        except Exception,e:
-            logging.exception(e)
-        """
-
 
     def __update_model_recursion__(self,model,brand_chinese,table_name):
         conn = Util().get_db_conn()
@@ -57,248 +45,161 @@ class UpdateModel(object):
                 update_sql = "update %s set model = '%s' WHERE url = '%s'"%(table_name,model,url)
                 cur.execute(update_sql)
                 conn.commit()
+                print 'update ----- ',model,brand_chinese,url
         # 如果找不到，则区别对待
         else:
-            print ''
-
-        res_title = update.get_result(conn,model)[0]
-        res_model = update.get_result(conn,model)[1]
-        data = open("conf/data.txt",'a')
-        print model,len(res_title),len(res_model)
-        # title字段可以找到，model字段找不到，则更新model字段为model
-        if len(res_title) > 0 and len(res_model) == 0:
-            for line in res_title:
-                url = line[2]
-                update_sql = "update shouji_all_spider_data set model = '%s' WHERE url = '%s'"%(model,url)
-                cur.execute(update_sql)
-                conn.commit()
-        elif len(res_title) == 0 and len(res_model) == 0:
             # 去掉HUAWEI开头查找华为的手机型号
+            data = open("conf/data_notfound.txt",'a')
             if model.startswith('HUAWEI '):
                 new_model = model.replace('HUAWEI ','').strip()
-                print new_model
-                res_title = update.get_result(conn,new_model)[0]
-                res_model = update.get_result(conn,new_model)[1]
-                if len(res_title) > 0 and len(res_model) == 0:
-                    data.write(model + ' ------ need update')
-                    data.write('\n')
-                    for line in res_title:
-                        url = line[2]
+                res = update.get_result(conn,brand_chinese,new_model)
+                if len(res) > 0:
+                    for line in res:
+                        url = line[0]
                         update_sql = "update shouji_all_spider_data set model = '%s' WHERE url = '%s'"%(model,url)
                         cur.execute(update_sql)
                         conn.commit()
-                elif len(res_title) == 0 and len(res_model) > 0:
-                    for line in res_model:
-                        url = line[2]
-                        update_sql = "update shouji_all_spider_data set model = '%s' WHERE url = '%s'"%(model,url)
-                        cur.execute(update_sql)
-                        conn.commit()
-                elif len(res_title) == 0 and len(res_model) == 0:
+                        print 'update ----- ',model,brand_chinese,url
+                else:
                     data.write(model + '---------- is not found')
                     data.write('\n')
             elif model.startswith('SM-'):
                 # 去掉SM-开头查找三星的手机型号
                 new_model = model.replace('SM-','').strip()
-                print new_model
-                res_title = update.get_result(conn,new_model)[0]
-                res_model = update.get_result(conn,new_model)[1]
-                if len(res_title) > 0 and len(res_model) == 0:
-                    for line in res_title:
-                        url = line[2]
+                res = update.get_result(conn,brand_chinese,new_model)
+                if len(res) > 0:
+                    for line in res:
+                        url = line[0]
                         update_sql = "update shouji_all_spider_data set model = '%s' WHERE url = '%s'"%(model,url)
                         cur.execute(update_sql)
                         conn.commit()
-                elif len(res_title) == 0 and len(res_model) > 0:
-                    for line in res_model:
-                        url = line[2]
-                        update_sql = "update shouji_all_spider_data set model = '%s' WHERE url = '%s'"%(model,url)
-                        cur.execute(update_sql)
-                        conn.commit()
-                elif len(res_title) == 0 and len(res_model) == 0:
+                        print 'update ----- ',model,brand_chinese,url
+                else:
                     data.write(model + '---------- is not found')
                     data.write('\n')
             elif model.startswith('GT-'):
                 # 去掉GT-开头查找三星的手机型号
                 new_model = model.replace('GT-','').strip()
-                print new_model
-                res_title = update.get_result(conn,new_model)[0]
-                res_model = update.get_result(conn,new_model)[1]
-                if len(res_title) > 0 and len(res_model) == 0:
-                    for line in res_title:
-                        url = line[2]
+                res = update.get_result(conn,brand_chinese,new_model)
+                if len(res) > 0:
+                    for line in res:
+                        url = line[0]
                         update_sql = "update shouji_all_spider_data set model = '%s' WHERE url = '%s'"%(model,url)
                         cur.execute(update_sql)
                         conn.commit()
-                elif len(res_title) == 0 and len(res_model) > 0:
-                    for line in res_model:
-                        url = line[2]
-                        update_sql = "update shouji_all_spider_data set model = '%s' WHERE url = '%s'"%(model,url)
-                        cur.execute(update_sql)
-                        conn.commit()
-                elif len(res_title) == 0 and len(res_model) == 0:
+                        print 'update ----- ',model,brand_chinese,url
+                else:
                     data.write(model + '---------- is not found')
                     data.write('\n')
             elif model.startswith('Coolpad '):
                 # Coolpad 开头查找酷派的手机型号
                 new_model = model.replace('Coolpad ','').strip()
-                print new_model
-                res_title = update.get_result(conn,new_model)[0]
-                res_model = update.get_result(conn,new_model)[1]
-                if len(res_title) > 0 and len(res_model) == 0:
-                    for line in res_title:
-                        url = line[2]
+                res = update.get_result(conn,brand_chinese,new_model)
+                if len(res) > 0:
+                    for line in res:
+                        url = line[0]
                         update_sql = "update shouji_all_spider_data set model = '%s' WHERE url = '%s'"%(model,url)
                         cur.execute(update_sql)
                         conn.commit()
-                elif len(res_title) == 0 and len(res_model) > 0:
-                    for line in res_model:
-                        url = line[2]
-                        update_sql = "update shouji_all_spider_data set model = '%s' WHERE url = '%s'"%(model,url)
-                        cur.execute(update_sql)
-                        conn.commit()
-                elif len(res_title) == 0 and len(res_model) == 0:
+                        print 'update ----- ',model,brand_chinese,url
+                else:
                     data.write(model + '---------- is not found')
                     data.write('\n')
             elif model.startswith('Lenovo '):
                 # Lenovo 开头查找联想的手机型号
                 new_model = model.replace('Lenovo ','').strip()
-                print new_model
-                res_title = update.get_result(conn,new_model)[0]
-                res_model = update.get_result(conn,new_model)[1]
-                if len(res_title) > 0 and len(res_model) == 0:
-                    for line in res_title:
-                        url = line[2]
+                res = update.get_result(conn,brand_chinese,new_model)
+                if len(res) > 0:
+                    for line in res:
+                        url = line[0]
                         update_sql = "update shouji_all_spider_data set model = '%s' WHERE url = '%s'"%(model,url)
                         cur.execute(update_sql)
                         conn.commit()
-                elif len(res_title) == 0 and len(res_model) > 0:
-                    for line in res_model:
-                        url = line[2]
-                        update_sql = "update shouji_all_spider_data set model = '%s' WHERE url = '%s'"%(model,url)
-                        cur.execute(update_sql)
-                        conn.commit()
-                elif len(res_title) == 0 and len(res_model) == 0:
+                        print 'update ----- ',model,brand_chinese,url
+                else:
                     data.write(model + '---------- is not found')
                     data.write('\n')
             elif model.startswith('HTC '):
                 # HTC 开头查找HTC的手机型号
                 new_model = model.replace('HTC ','').strip()
-                print new_model
-                res_title = update.get_result(conn,new_model)[0]
-                res_model = update.get_result(conn,new_model)[1]
-                if len(res_title) > 0 and len(res_model) == 0:
-                    for line in res_title:
-                        url = line[2]
+                res = update.get_result(conn,brand_chinese,new_model)
+                if len(res) > 0:
+                    for line in res:
+                        url = line[0]
                         update_sql = "update shouji_all_spider_data set model = '%s' WHERE url = '%s'"%(model,url)
                         cur.execute(update_sql)
                         conn.commit()
-                elif len(res_title) == 0 and len(res_model) > 0:
-                    for line in res_model:
-                        url = line[2]
-                        update_sql = "update shouji_all_spider_data set model = '%s' WHERE url = '%s'"%(model,url)
-                        cur.execute(update_sql)
-                        conn.commit()
-                elif len(res_title) == 0 and len(res_model) == 0:
+                        print 'update ----- ',model,brand_chinese,url
+                else:
                     data.write(model + '---------- is not found')
                     data.write('\n')
             elif model.startswith('ZTE '):
                 # ZTE 开头查找中兴的手机型号
                 new_model = model.replace('ZTE ','').strip()
-                print new_model
-                res_title = update.get_result(conn,new_model)[0]
-                res_model = update.get_result(conn,new_model)[1]
-                if len(res_title) > 0 and len(res_model) == 0:
-                    for line in res_title:
-                        url = line[2]
+                res = update.get_result(conn,brand_chinese,new_model)
+                if len(res) > 0:
+                    for line in res:
+                        url = line[0]
                         update_sql = "update shouji_all_spider_data set model = '%s' WHERE url = '%s'"%(model,url)
                         cur.execute(update_sql)
                         conn.commit()
-                elif len(res_title) == 0 and len(res_model) > 0:
-                    for line in res_model:
-                        url = line[2]
-                        update_sql = "update shouji_all_spider_data set model = '%s' WHERE url = '%s'"%(model,url)
-                        cur.execute(update_sql)
-                        conn.commit()
-                elif len(res_title) == 0 and len(res_model) == 0:
+                        print 'update ----- ',model,brand_chinese,url
+                else:
                     data.write(model + '---------- is not found')
                     data.write('\n')
             elif model.startswith('koobee '):
                 # koobee 开头查找酷比的手机型号
                 new_model = model.replace('koobee ','').strip()
-                print new_model
-                res_title = update.get_result(conn,new_model)[0]
-                res_model = update.get_result(conn,new_model)[1]
-                if len(res_title) > 0 and len(res_model) == 0:
-                    for line in res_title:
-                        url = line[2]
+                res = update.get_result(conn,brand_chinese,new_model)
+                if len(res) > 0:
+                    for line in res:
+                        url = line[0]
                         update_sql = "update shouji_all_spider_data set model = '%s' WHERE url = '%s'"%(model,url)
                         cur.execute(update_sql)
                         conn.commit()
-                elif len(res_title) == 0 and len(res_model) > 0:
-                    for line in res_model:
-                        url = line[2]
-                        update_sql = "update shouji_all_spider_data set model = '%s' WHERE url = '%s'"%(model,url)
-                        cur.execute(update_sql)
-                        conn.commit()
-                elif len(res_title) == 0 and len(res_model) == 0:
+                        print 'update ----- ',model,brand_chinese,url
+                else:
                     data.write(model + '---------- is not found')
                     data.write('\n')
             elif model.startswith('vivo '):
                 # vivo 开头查找vivo的手机型号
                 new_model = model.replace('vivo ','').strip()
-                print new_model
-                res_title = update.get_result(conn,new_model)[0]
-                res_model = update.get_result(conn,new_model)[1]
-                if len(res_title) > 0 and len(res_model) == 0:
-                    for line in res_title:
-                        url = line[2]
+                res = update.get_result(conn,brand_chinese,new_model)
+                if len(res) > 0:
+                    for line in res:
+                        url = line[0]
                         update_sql = "update shouji_all_spider_data set model = '%s' WHERE url = '%s'"%(model,url)
                         cur.execute(update_sql)
                         conn.commit()
-                elif len(res_title) == 0 and len(res_model) > 0:
-                    for line in res_model:
-                        url = line[2]
-                        update_sql = "update shouji_all_spider_data set model = '%s' WHERE url = '%s'"%(model,url)
-                        cur.execute(update_sql)
-                        conn.commit()
-                elif len(res_title) == 0 and len(res_model) == 0:
+                        print 'update ----- ',model,brand_chinese,url
+                else:
                     data.write(model + '---------- is not found')
                     data.write('\n')
             elif model.startswith('ZUK '):
                 # ZUK 开头查找ZUK的手机型号
                 new_model = model.replace('ZUK ','').strip()
-                print new_model
-                res_title = update.get_result(conn,new_model)[0]
-                res_model = update.get_result(conn,new_model)[1]
-                if len(res_title) > 0 and len(res_model) == 0:
-                    for line in res_title:
-                        url = line[2]
+                res = update.get_result(conn,brand_chinese,new_model)
+                if len(res) > 0:
+                    for line in res:
+                        url = line[0]
                         update_sql = "update shouji_all_spider_data set model = '%s' WHERE url = '%s'"%(model,url)
                         cur.execute(update_sql)
                         conn.commit()
-                elif len(res_title) == 0 and len(res_model) > 0:
-                    for line in res_model:
-                        url = line[2]
-                        update_sql = "update shouji_all_spider_data set model = '%s' WHERE url = '%s'"%(model,url)
-                        cur.execute(update_sql)
-                        conn.commit()
-                elif len(res_title) == 0 and len(res_model) == 0:
+                        print 'update ----- ',model,brand_chinese,url
+                else:
                     data.write(model + '---------- is not found')
                     data.write('\n')
             else:
                 data.write(model + '---------- is not found')
                 data.write('\n')
 
-    def get_result(self,conn,model):
+    def get_result(self,conn,brand_chinese,model):
         cur = conn.cursor()
-        sql_title = "select title,model,url from shouji_all_spider_data WHERE title LIKE '%%%%%s%%%%'"%model
+        sql = "select url from shouji_all_spider_data WHERE title LIKE '%%%%%s%%%%' AND (title LIKE '%%%%%s%%%%' OR model LIKE '%%%%%s%%%%')"%(brand_chinese,model,model)
         # model本身是否存在该手机型号
-        sql_model = "select title,model,url from shouji_all_spider_data WHERE model LIKE '%%%%%s%%%%'"%model
-        cur.execute(sql_title)
-        res_title = cur.fetchall()
-        cur.execute(sql_model)
-        res_model = cur.fetchall()
-        return res_title,res_model
+        cur.execute(sql)
+        res = cur.fetchall()
+        return res
 
 
 if __name__ == '__main__':
