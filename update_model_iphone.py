b975f36b2d9c10246716f37d4105204be9203de4
lftuo <909709223@qq.com>
2017-11-10 10:36:09 +0800

    修改代码注释

--- update_model_iphone.py
+++ update_model_iphone.py
@@ -9,12 +9,14 @@ from util import Util
 
 
 class UpdateIphoneModel(object):
-    """
-    更新iPhone手机型号表，iPhone手机型号表iphone_model，参考网页：https://www.theiphonewiki.com/wiki/Models
-    实现逻辑：查找iphone_model中的phone_name字段，然后模糊匹配爬虫数据表中的title字段，匹配成功，则修改爬虫数据的model字段为iphone_model中Internal_Name与Identifier字段拼接之和
-    :param table_name 爬虫数据表
-    """
     def __update__(self,table_name):
+        '''
+        更新iPhone手机型号表，iPhone手机型号表iphone_model，参考网页：https://www.theiphonewiki.com/wiki/Models
+        实现逻辑：查找iphone_model中的phone_name字段，然后模糊匹配爬虫数据表中的title字段，匹配成功，则修改爬虫数据的model字段为iphone_model中Internal_Name与Identifier字段拼接之和
+        :param table_name:爬虫数据表
+        :return:
+        '''
+
         # 将iPhone的手机型号置空
         conn = Util().get_db_conn()
         cur = conn.cursor()
@@ -22,9 +24,11 @@ class UpdateIphoneModel(object):
         cur.execute(models_sql)
         models = cur.fetchall()
         for line in models:
+            # iPhone手机名称
             phone_name = line[0]
-            type = line[1]
+            # iPhone手机型号1
             Internal_Name = line[2]
+            # iPhone手机型号2
             Identifier = line[3]
             result_sql = "select url from %s where title LIKE '%%%%%s%%%%'"%(table_name,phone_name)
             cur.execute(result_sql)
